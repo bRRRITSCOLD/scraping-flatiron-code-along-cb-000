147625c21f7de7340e9b8021a53ba049ab4d38aa
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-28 18:03:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,23 @@ require_relative './course.rb'
 
 class Scraper
 
-    def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-      binding.pry
+    binding.pry
+  end
+  
+  def print_courses
+  self.make_courses
+  Course.all.each do |course|
+    if course.title
+      puts "Title: #{course.title}"
+      puts "  Schedule: #{course.schedule}"
+      puts "  Description: #{course.description}"
     end
+  end
+
 end
 
-lol = Scraper.new
-lol.get_page
+Scraper.new.get_page
+
